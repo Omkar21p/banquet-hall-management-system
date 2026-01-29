378baa3011605dab70600e54930ce3f167d4ae70
Omkar21p <135224879+Omkar21p@users.noreply.github.com>
2026-01-29 21:48:52 +0530

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,23 +1,9 @@
 const path = require("path");
 
 module.exports = {
-  // Disable ESLint completely during production build
-  eslint: {
-    enable: false,
-  },
-
   webpack: {
     alias: {
       "@": path.resolve(__dirname, "src"),
-    },
-
-    configure: (webpackConfig) => {
-      // Remove ESLintWebpackPlugin entirely (fixes Netlify CI failures)
-      webpackConfig.plugins = webpackConfig.plugins.filter(
-        (plugin) => plugin.constructor.name !== "ESLintWebpackPlugin"
-      );
-
-      return webpackConfig;
     },
   },
 };
